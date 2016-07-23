0605a15f7455e6976b0a13fb12fc0fd5397aaf1d
André Gröschel <andre.groeschel@gmail.com>
2016-07-23 18:34:16 +0200

    Adds water pool

---
+++ Assets/_Scripts/WaterBalloon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBalloon : MonoBehaviour
+{
+    public int capacity;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //characterInQuicksand = true;
+    }
+}
