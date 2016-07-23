9e31e2b48ca80f6a05e4dea152d486f23de50aee
jura <jura@Maximilians-MacBook-Pro.local>
2016-07-23 17:43:09 +0200

    bullets 2d

--- Assets/_Scripts/PlayerNetworkMove.cs
+++ Assets/_Scripts/PlayerNetworkMove.cs
@@ -2,26 +2,67 @@
 using System.Collections;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerNetworkMove : NetworkBehaviour {
 
 	public float moveSpeed = 5f;
+	public GameObject bullet;
 
-	Rigidbody2D body; // Used to cache the RigidBody
+	Collider2D bodyCollider;
+	Rigidbody2D body;
 	float hInput;
 	float vInput;
 
+	[Command]
+	void CmdFire(Vector3 shootPosition)
+	{
+		RpcFire(shootPosition);
+	}
+
+	[ClientRpc]
+	void RpcFire(Vector3 shootPosition)
+	{
+		if(isLocalPlayer) return;
+
+		SpawnBullet (this.transform.position, shootPosition);
+	}
+
+	void Fire(Vector3 shootPosition)
+	{
+		if(isLocalPlayer == false) return;
+
+		SpawnBullet (this.transform.position, shootPosition);
+	}
+		
+	void SpawnBullet(Vector3 pos, Vector3 shootPosition)
+	{
+		GameObject bulletGO = NetworkBehaviour.Instantiate (bullet, pos, Quaternion.identity) as GameObject;
+		var forward = (shootPosition - pos).normalized;
+		//bulletGO.transform.forward = forward;
+		bulletGO.GetComponent<Rigidbody2D> ().velocity = forward * 10.0f;
+		Physics2D.IgnoreCollision (bodyCollider, bulletGO.GetComponent<Collider2D> ());
+	}
+
 	void Awake()
 	{
+		bodyCollider = GetComponent<Collider2D> ();
 		body = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update () {
 		if(isLocalPlayer == false) return; // Don't check input if this isn't
 		// running on the local player object
 		hInput = Input.GetAxis("Horizontal");
 		vInput = Input.GetAxis("Vertical");
+
+		if (Input.GetMouseButton (0)) {
+			var worldMousePosition = Input.mousePosition;
+			worldMousePosition.z = 10.0f;
+			worldMousePosition = Camera.main.ScreenToWorldPoint(worldMousePosition);
+			Fire (worldMousePosition);
+			CmdFire (worldMousePosition);
+		}
 	}
 
 	void FixedUpdate()
@@ -31,7 +72,7 @@ public class PlayerNetworkMove : NetworkBehaviour {
 
 		// Remove unwanted forces resulting from collisions
 		body.velocity = Vector3.zero;
-		body.angularVelocity = 0f;
+		body.angularVelocity = 0;
 
 		var mov = new Vector2 (hInput, vInput);
 
